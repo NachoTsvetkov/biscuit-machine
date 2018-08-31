b6d5ef9a1aed29629f607c819b5254c2c050b7a5
nacho.tsvetkov <nacho.tsvetkov@gmail.com>
2018-08-31 05:49:03 +0300

    stamper tests implemented

--- BiscuitMaker/BiscuitMaker.Tests/Managers/StamperTests.cs
+++ BiscuitMaker/BiscuitMaker.Tests/Managers/StamperTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using FluentAssertions;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,37 @@ using System.Threading.Tasks;
 namespace BiscuitMaker.Tests.Managers
 {
     [TestFixture]
-    class StamperTests
+    class StamperTests : ManagerTestsBase
     {
+        [Test]
+        public void ExtrudeTest()
+        {
+            Action action = () => Stamper.Stamp(Biscuit.Create());
+
+            action.Should().NotThrow();
+
+            var biscuit = Stamper.Stamp(Biscuit.Create(true, false, false));
+            biscuit.IsExtruded.Should().BeTrue();
+            biscuit.IsStamped.Should().BeTrue();
+            biscuit.IsDone.Should().BeFalse();
+        }
+
+        [Test]
+        public void HandleMotorPulseTest()
+        {
+            this.Maker.FirstConveyor.Belt.Insert(1, Biscuit.Create(true, false, false));
+            Action action = () => Stamper.HandleMotorPulse(
+                null,
+                new OnMotorPulseEventArgs { Maker = this.Maker }
+            );
+
+            action.Should().NotThrow();
+
+            var biscuit = this.Maker.FirstConveyor.Belt.ElementAt(1);
+            biscuit.Should().NotBeNull();
+            biscuit.IsExtruded.Should().BeTrue();
+            biscuit.IsStamped.Should().BeTrue();
+            biscuit.IsDone.Should().BeFalse();
+        }
     }
 }
